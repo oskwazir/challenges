5bbdc87bc2abab889745939bbe0343f09fec5e38
Omer Wazir <omer@thewazir.com>
2016-06-23 21:07:18 -0700

    WIP no solution yet

--- elixir/talantdetector/lib/talantdetector.ex
+++ elixir/talantdetector/lib/talantdetector.ex
@@ -1,2 +1,7 @@
 defmodule TalantDetector do
+
+	@spec are_you_playing_banjo?(charlist()) :: charlist()
+	def are_you_playing_banjo?(name) do
+		"#{name} does not play banjo"
+	end
 end
--- elixir/talantdetector/test/talantdetector_test.exs
+++ elixir/talantdetector/test/talantdetector_test.exs
@@ -1,8 +1,11 @@
 defmodule TalantDetectorTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest TalantDetector
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import TalantDetector, only: [are_you_playing_banjo?: 1]
+
+  test "play and don't play banjo" do
+    assert are_you_playing_banjo?("Martin") == "Martin does not play banjo"
+    assert are_you_playing_banjo?("Rikke") == "Rikke plays banjo"
   end
 end
